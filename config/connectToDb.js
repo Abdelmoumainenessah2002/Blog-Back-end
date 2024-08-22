a345e61eb911f848e4ec049791d1179a67af2f75
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2024-08-22 16:02:43 +0100

    Add config directory with connectToDb file for database connection

---
+++ config/connectToDb.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+module.exports = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Connected to mongo db");
+    } catch (error) {
+        console.log("Connection Failed ", error);
+    }
+}
