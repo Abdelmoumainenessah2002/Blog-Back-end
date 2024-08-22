78052e9edf57ca0fb02e6b593573449085f9598d
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2024-08-22 15:57:34 +0100

    Add app.js with initial server setup

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const connectToDb = require("./config/connectToDb");
+require("dotenv").config();
+
+// Conection To DB
+connectToDb();
+
+// Init App
+const app = express();
+
+//Middleware
+
+app.use(express.json()); // this function let the express know the json file which send from the client
+
+
+// routes
+app.use("/api/auth", require("./routes/authRoute"));
+
+// Running The server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () =>
+  console.log(
+    `server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
