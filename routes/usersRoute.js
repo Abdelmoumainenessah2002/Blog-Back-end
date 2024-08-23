90e4622f7adbaa9c5ed736e361d53e8f32de69dd
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2024-08-24 00:34:42 +0100

    Add the Upload photo route handler

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,12 +1,19 @@
 const router = require("express").Router();
-const { getAllUsersCtrl, getUserProfileCtrl, updateUserProfileCtrl, getUsersCountCtrl } = require("../controllers/usersController");
-const {verifyTokenAndAdmin, verifyTokenAnd1OnlyUser} = require("../middlewares/verifyToken");
+const { getAllUsersCtrl, getUserProfileCtrl, updateUserProfileCtrl, getUsersCountCtrl, profilePhotoUploadCtrl } = require("../controllers/usersController");
+const {verifyTokenAndAdmin, verifyTokenAnd1OnlyUser, verifyToken} = require("../middlewares/verifyToken");
 const validateObjectID = require("../middlewares/validateObjectID");
+const photoUpload = require("../middlewares/photoUpload");
 
 // /api/users/profile
 router.route("/profile").get(verifyTokenAndAdmin, getAllUsersCtrl);
+
+// /api/users/profile/:id
 router.route("/profile/:id").get(validateObjectID, getUserProfileCtrl)
                             .put(validateObjectID,verifyTokenAnd1OnlyUser, updateUserProfileCtrl);
+
+// /api/users/profile/profile-photo-upload
+router.route("/profile/profile-photo-upload")
+      .post(verifyToken, photoUpload.single("image"), profilePhotoUploadCtrl); // Single is used to upload single file
 
 
 // api/users/count
