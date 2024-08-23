d477abb2464d5b7e6d7baeffbfdf40f2d25a2d32
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2024-08-23 13:59:10 +0100

    Add validateObjectID middleware for ObjectID validation

--- middlewares/validateObjectID.js
+++ middlewares/validateObjectID.js
@@ -4,5 +4,5 @@ module.exports = (req, res, next) => {
     if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
         return res.status(400).json({message: "Invalid Object ID"});
     }
-    next();
+    next();  
 }
