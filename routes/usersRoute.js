ce4429f0f16788b82b45bb1d4ceabed342172cbf
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2024-08-24 16:23:54 +0100

    add the upload profile photo and delete profile controllers to the route

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,6 +1,14 @@
 const router = require("express").Router();
-const { getAllUsersCtrl, getUserProfileCtrl, updateUserProfileCtrl, getUsersCountCtrl, profilePhotoUploadCtrl } = require("../controllers/usersController");
-const {verifyTokenAndAdmin, verifyTokenAnd1OnlyUser, verifyToken} = require("../middlewares/verifyToken");
+const {
+  getAllUsersCtrl,
+  getUserProfileCtrl,
+  updateUserProfileCtrl,
+  getUsersCountCtrl,
+  profilePhotoUploadCtrl,
+  deleteUserProfileCtrl,
+} = require("../controllers/usersController");
+
+const {verifyTokenAndAdmin, verifyTokenAndOnlyUser, verifyToken, verifyTokenAndAuthorization} = require("../middlewares/verifyToken");
 const validateObjectID = require("../middlewares/validateObjectID");
 const photoUpload = require("../middlewares/photoUpload");
 
@@ -9,8 +17,8 @@ router.route("/profile").get(verifyTokenAndAdmin, getAllUsersCtrl);
 
 // /api/users/profile/:id
 router.route("/profile/:id").get(validateObjectID, getUserProfileCtrl)
-                            .put(validateObjectID,verifyTokenAnd1OnlyUser, updateUserProfileCtrl);
-
+                            .put(validateObjectID,verifyTokenAndOnlyUser, updateUserProfileCtrl)
+                            .delete(validateObjectID,verifyTokenAndAuthorization, deleteUserProfileCtrl);
 // /api/users/profile/profile-photo-upload
 router.route("/profile/profile-photo-upload")
       .post(verifyToken, photoUpload.single("image"), profilePhotoUploadCtrl); // Single is used to upload single file
