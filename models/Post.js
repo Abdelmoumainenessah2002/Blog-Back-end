694296390611526dc2c7be4d171dc7a1770fb038
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2024-08-24 16:25:22 +0100

    Add the Post Schema

---
+++ models/Post.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+// Post Schema
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 6,
+        maxlength: 255
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 10,
+        maxlength: 1024
+    },
+    user : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    image : {
+        type: Object,
+        default: {
+            url: '',
+            publicId: null
+        },
+        required: true
+    },
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }],
+}, { timestamps: true });
+
+
+// Post Model
+const Post = mongoose.model('Post', postSchema);
+
+
+// Validate Create Post
+function validateCreatePost(post) {
+    const schema = Joi.object({
+      title: Joi.string().min(6).max(255).trim().required(),
+      description: Joi.string().min(10).trim().required(),
+      category: Joi.string().trim().required(),
+    });
+    return schema.validate(post);
+}
+
+// Validate Update Post
+function validateUpdatePost(post) {
+    const schema = Joi.object({
+      title: Joi.string().min(6).max(255).trim(),
+      description: Joi.string().min(10).trim(),
+      category: Joi.string().trim(),
+    });
+    return schema.validate(post);
+}
+
+module.exports = {
+    Post,
+    validateCreatePost,
+    validateUpdatePost
+}
