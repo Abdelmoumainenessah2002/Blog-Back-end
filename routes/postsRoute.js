4d4f2e9b24e5f9d56dd50e51902f429e71698269
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2024-08-25 13:30:57 +0100

    add the likes route

--- routes/postsRoute.js
+++ routes/postsRoute.js
@@ -8,6 +8,10 @@ const {
   getAllPostsCtrl,
   getSinglePostCtrl,
   getPostsCountCtrl,
+  deletePostCtrl,
+  updatePostCtrl,
+  updatePostImageCtrl,
+  toggleLikeCtrl,
 } = require("../controllers/postController");
 
 const validateObjectID = require("../middlewares/validateObjectID");
@@ -23,9 +27,18 @@ router.route("/").post(verifyToken, photoUpload.single("image"), createPostCtrl)
 router.route("/count").get(getPostsCountCtrl);
 
 // /api/posts/:id
-router.route("/:id").get(validateObjectID, getSinglePostCtrl);
+router.route("/:id").get(validateObjectID, getSinglePostCtrl)
+                    .delete(validateObjectID, verifyToken, deletePostCtrl)
+                    .put(validateObjectID, verifyToken, updatePostCtrl);
 
 
+
+// /api/posts/upload-image/:id
+router.route("/update-image/:id").put(validateObjectID, verifyToken, photoUpload.single("image"), updatePostImageCtrl);
+
+
+// /api/posts/like/:id
+router.route("/like/:id").put(validateObjectID, verifyToken, toggleLikeCtrl);
 
 // export the router
 module.exports = router;
