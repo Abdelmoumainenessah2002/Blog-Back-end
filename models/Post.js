67a43fbd8421619258a53c0593432316b96912f5
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2024-08-25 19:25:16 +0100

    Add The virtual relation between Post and Comment

--- models/Post.js
+++ models/Post.js
@@ -2,45 +2,56 @@ const mongoose = require('mongoose');
 const Joi = require('joi');
 
 // Post Schema
-const postSchema = new mongoose.Schema({
+const postSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 6,
-        maxlength: 255
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 6,
+      maxlength: 255,
     },
     description: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 10,
-        maxlength: 1024
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 10,
+      maxlength: 1024,
     },
-    user : {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
     category: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    image : {
-        type: Object,
-        default: {
-            url: '',
-            publicId: null
-        },
-        required: true
+    image: {
+      type: Object,
+      default: {
+        url: "",
+        publicId: null,
+      },
+      required: true,
     },
     likes: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User'
-        }],
-}, { timestamps: true });
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
 
+
+// Post Schema Virtuals
+postSchema.virtual('comments', {
+    ref: 'Comment',
+    foreignField: 'postId',
+    localField: '_id'
+});
 
 // Post Model
 const Post = mongoose.model('Post', postSchema);
