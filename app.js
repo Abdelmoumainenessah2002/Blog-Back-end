3f662a3de9a0d636138ea12fba2e44246ba1b56a
abdelmoumainenessah2002 <abdelmoumainenessah@gmail.com>
2024-09-03 04:16:42 +0100

    add the hemlet hpp and xss

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const connectToDb = require("./config/connectToDb");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const hpp = require("hpp");
 const { notFound, errorHandler } = require("./middlewares/error");
+const cors = require("cors");
 require("dotenv").config();
 
 // Conection To DB
@@ -9,10 +14,31 @@ connectToDb();
 // Init App
 const app = express();
 
+// Security (Helmet)
+app.use(helmet());
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate Limiting
+app.use(
+  rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 200,
+  })
+);
+
 //Middleware
 
 app.use(express.json()); // this function let the express know the json file which send from the client
 
+// cors policy
+app.use(cors({
+  origin: "http://localhost:3000"
+}));
 
 // routes
 app.use("/api/auth", require("./routes/authRoute"));
@@ -20,6 +46,7 @@ app.use("/api/users", require("./routes/usersRoute"));
 app.use("/api/posts", require("./routes/postsRoute"));
 app.use("/api/comments", require("./routes/commentsRoute"));
 app.use("/api/categories", require("./routes/categoriesRoute"));
+app.use("/api/password", require("./routes/passwordRoute"));
 
 
 
